43684739b5f235119b11f566bbe47b8c81e298c7
Alex Merecka <merecka@gmail.com>
2019-03-25 17:25:13 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,16 @@
-def starts_with_a_vowel?(word)
+require 'pry'
 
+def starts_with_a_vowel?(word)
+/\A[aeiouAEIOU]/.match?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+#    text.scan(/\A+un\w+ing/)
+  text.scan(/\A*un\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.split(" ").grep(/\A[a-zA-Z]{5}\z/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
